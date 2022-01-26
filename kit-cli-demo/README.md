cbc7d4c3dac00ce6bd87c215f62f1907fc744a33
daniellin <s102033114@gapp.nthu.edu.tw>
2022-01-27 02:46:51 +0800

    feat(kit): generate middleware

--- kit-cli-demo/README.md
+++ kit-cli-demo/README.md
@@ -11,4 +11,9 @@ WARN[0000] You also need to implement the Encoders and Decoders!
 WARN[0000] ===============================================================
 
 kit g m hi -s hello
+
+// For endpoint
+kit g m hi -s hello -e
+INFO[0000] Do not forget to append your endpoint middleware to your service middlewares
+INFO[0000] Add it to cmd/service/service.go#getEndpointMiddleware()
 ```
--- kit-cli-demo/hello/pkg/endpoint/middleware.go
+++ kit-cli-demo/hello/pkg/endpoint/middleware.go
@@ -3,10 +3,11 @@ package endpoint
 import (
 	"context"
 	"fmt"
+	"time"
+
 	endpoint "github.com/go-kit/kit/endpoint"
 	log "github.com/go-kit/kit/log"
 	metrics "github.com/go-kit/kit/metrics"
-	"time"
 )
 
 // InstrumentingMiddleware returns an endpoint middleware that records
@@ -32,6 +33,16 @@ func LoggingMiddleware(logger log.Logger) endpoint.Middleware {
 			defer func(begin time.Time) {
 				logger.Log("transport_error", err, "took", time.Since(begin))
 			}(time.Now())
+			return next(ctx, request)
+		}
+	}
+}
+
+// HiMiddleware returns an endpoint middleware
+func HiMiddleware() endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+			// Add your middleware logic here
 			return next(ctx, request)
 		}
 	}
